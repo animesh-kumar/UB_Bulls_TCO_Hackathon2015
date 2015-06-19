79e8192356e9501c68685bed027f06fae4bfc015
Mani Kishore Chitrala <mchitrala@vmware.com>
2015-06-18 20:25:17 -0700

    Initial Commit

---
+++ Stadium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stadium
+//
+//  Created by Mani Kishore Chitrala on 6/18/15.
+//  Copyright (c) 2015 Mani Kishore Chitrala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Stadium/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stadium
+//
+//  Created by Mani Kishore Chitrala on 6/18/15.
+//  Copyright (c) 2015 Mani Kishore Chitrala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
